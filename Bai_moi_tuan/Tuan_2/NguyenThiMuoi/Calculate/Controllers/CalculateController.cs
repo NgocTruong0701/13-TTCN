f2e9d056d89da97e6a7d3774aab96850053d3500
muoi2905 <muoips2002@gmail.com>
2023-04-02 16:28:09 +0700

    calculator

---
+++ Bai_moi_tuan/Tuan_2/NguyenThiMuoi/Calculate/Controllers/CalculateController.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calculate.Controllers
+{
+    public class CalculateController : Controller
+    {
+        // GET: Calculate
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(double a, double b, string PhepTinh = "+")
+        {
+            switch (PhepTinh)
+            {
+                case "+":
+                    ViewBag.Ketqua = string.Format("{0:0.##} + {1:0.##} = {2:0.##}", a, b, (a+b));
+                    break;
+                case "-":
+                    ViewBag.Ketqua = string.Format("{0:0.##} - {1:0.##} = {2:0.##}", a, b,( a - b));
+                    break;
+                case "*":
+                    ViewBag.Ketqua = string.Format("{0:0.##} * {1:0.##} = {2:0.##}", a, b,( a * b));
+                    break;
+                case "/":
+                    if (b == 0)
+                    {
+                        ViewBag.Ketqua = "Khong chia duoc cho 0";
+                        break;
+                    }
+                    ViewBag.Ketqua = string.Format("{0:0.##} * {1:0.##} = {2:0.##}", a, b, (a / b));
+                    break;
+                default:
+                    break;
+            }
+            return View("Index");
+        }
+        
+        
+
+    }
+}
---
+++ Bai_moi_tuan/Tuan_2/NguyenThiMuoi/Calculate/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Calculate.MvcApplication" Language="C#" %>
