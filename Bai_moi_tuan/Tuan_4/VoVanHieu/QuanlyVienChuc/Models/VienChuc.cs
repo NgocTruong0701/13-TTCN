b7e3f1bd64ac7e5d3279b72178929db537617a86
Hieu <hieumhna@gmail.com>
2023-04-09 13:21:58 +0700

    first commit

---
+++ Bai_moi_tuan/Tuan_4/VoVanHieu/QuanlyVienChuc/Models/VienChuc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace de5.Models
+{
+    public partial class VienChuc
+    {
+        public string Mavc { get; set; }
+        public string Hoten { get; set; }
+        public string Madv { get; set; }
+        public int? Songaycong { get; set; }
+
+        public virtual DonVi MadvNavigation { get; set; }
+    }
+}
