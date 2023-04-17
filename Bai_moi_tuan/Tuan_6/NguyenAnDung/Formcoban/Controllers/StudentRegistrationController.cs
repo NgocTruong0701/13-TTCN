69c36334b541df84720597df8d4ff536fbd07405
dũng nguyễn <andungkoi@gmail.com>
2023-04-17 12:48:10 +0700

    taibaitap

---
+++ Bai_moi_tuan/Tuan_6/NguyenAnDung/Formcoban/Controllers/StudentRegistrationController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Formcoban.Models;
+
+namespace Formcoban.Controllers
+{
+    public class StudentRegistrationController : Controller
+    {
+        // GET: StudentRegistration
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Regis(Student s)
+        {
+            return View(s);
+        }
+
+        public ActionResult Regis2(FormCollection f)
+        {
+            Student s = new Student();
+            s.name = f["name"];
+            s.gender = f["gender"];
+            s.email = f["email"];
+            s.addr = f["addr"];
+
+            string temp = "";
+            if (f["Java core"] == "true,false")
+                temp = "Java core";
+            if (f["SQL Server"] == "true,false")
+                temp += "SQL Server";
+            if (f["PHP"] == "true,false")
+                temp += "PHP";
+
+            s.subjects = temp;
+
+            s.username = f["username"];
+            s.password = f["password"];
+            s.comment = f["comment"];
+
+            return View(s);
+        }
+
+        public ActionResult Bai2_Form()
+        {
+            return View();
+        }
+        public ActionResult Bai2_Detail(FormCollection f)
+        {
+            Student s = new Student();
+            s.fname = f["fname"];
+            s.lname = f["lname"];
+            s.email = f["email"];
+            s.password = f["password"];
+            s.addr = f["addr"];
+            s.gender = f["gender"];
+
+            return View(s);
+        }
+    }
+}
---
+++ Bai_moi_tuan/Tuan_6/NguyenAnDung/Formcoban/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Formcoban.Models
+{
+    public class Student
+    {
+        public string name { get; set; }
+        public string gender { get; set; }
+        public string email { get; set; }
+        public string addr { get; set; }
+        public string subjects { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string comment { get; set; }
+        public string fname { get; set; }
+        public string lname { get; set; }
+    }
+}
