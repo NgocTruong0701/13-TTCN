cee57c2f0cacf8fbae9e22d7c3f7a2c99698c559
muoi2905 <muoips2002@gmail.com>
2023-04-21 21:40:51 +0700

    bt tuan 6

---
+++ Bai_moi_tuan/Tuan_6/NguyenThiMuoi/Controllers/SanPhamController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using De5.Models;
+
+namespace De5.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        // GET: SanPham
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DanhSachSanPham()
+        {
+            List<SanPham> li = new List<SanPham>();
+            li.Add(new SanPham("s01", "San pham 1", 10, 100, 0));
+            li.Add(new SanPham("s02", "San pham 2", 20, 120, 0));
+            li.Add(new SanPham("s03", "San pham 3", 15, 200, 0));
+            li.Add(new SanPham("s04", "San pham 4", 30, 150, 0));
+            li.Add(new SanPham("s04", "San pham 4", 20, 50, 0));
+            return View(li);
+
+        }
+        public ActionResult HienThiDS()
+        {
+            int giatien = int.Parse(Request["giatien"]);
+            if (giatien > 100)
+            {
+                List<SanPham> li1 = new List<SanPham>();
+                li1.Add(new SanPham("s01", "San pham 1", 10, 100, 0));
+                li1.Add(new SanPham("s02", "San pham 2", 20, 120, 0));
+                li1.Add(new SanPham("s03", "San pham 3", 15, 200, 0));
+                li1.Add(new SanPham("s04", "San pham 4", 30, 150, 0));
+                li1.Add(new SanPham("s04", "San pham 4", 20, 50, 0));
+            }
+
+            int giamgia = int.Parse(Request["giamgia"]);
+            if (giamgia == 1)
+            {
+                List<SanPham> li2 = new List<SanPham>();
+                li2.Add(new SanPham("s01", "San pham 1", 10, 100, 0));
+                li2.Add(new SanPham("s02", "San pham 2", 20, 120, 0));
+                li2.Add(new SanPham("s03", "San pham 3", 15, 200, 0));
+                li2.Add(new SanPham("s04", "San pham 4", 30, 150, 0));
+                li2.Add(new SanPham("s04", "San pham 4", 20, 50, 0));
+            }
+
+            ViewBag.tien = giatien;
+            ViewBag.giamgia = giamgia;
+            return View();
+            
+
+
+
+        }
+
+        public ActionResult NhapSP()
+        {
+            return View();
+        }    
+
+        public ActionResult HienThiSP(SanPham s)
+        {
+            return View(s);
+        }
+
+
+    }
+}
---
+++ Bai_moi_tuan/Tuan_6/NguyenThiMuoi/Models/SanPham.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+
+namespace De5.Models
+{
+    public class SanPham
+    {
+
+        [DisplayName("Mã sản phẩm: ")]
+        public string masp { get; set; }
+
+        [DisplayName("Tên sản phẩm: ")]
+        public string tensp { get; set; }
+
+        [DisplayName("Số lượng: ")]
+        public int soluong { get; set; }
+
+        [DisplayName("Giá tiền: ")]
+        public int giatien { get; set; }
+
+        [DisplayName("Giảm giá: ")]
+        public int giamgia { get; set; }
+
+       public double thanhtien
+        {
+            get
+            {
+                if (giamgia == 0)
+                    return soluong * giatien;
+                else
+                    return soluong * giatien * 0.9;
+            }
+        } 
+
+        public SanPham()
+        {
+
+        }
+
+        public SanPham(string masp, string tensp, int soluong, int giatien, int giamgia)
+        {
+            this.masp = masp;
+            this.tensp = tensp;
+            this.soluong = soluong;
+            this.giatien = giatien;
+            this.giamgia = giamgia;
+            
+            
+
+        }
+
+      
+
+    }
+}
